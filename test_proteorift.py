8a1570307f2ac8f592e4dc230e101149e375dbb7
bilals08 <bilalshabbir>
2026-03-17 02:21:05 -0400

    pypi package updated

---
+++ test_proteorift.py
@@ -0,0 +1,10 @@
+import proteorift
+
+
+from proteorift import ProteoRiftSearch
+
+# Initialize and run with sample data
+searcher = ProteoRiftSearch()
+results = searcher.search_with_sample_data()
+
+
